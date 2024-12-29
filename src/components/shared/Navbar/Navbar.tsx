9feda1e790173d385542ed2469caa83821b366d1
mahmudtrainer <mahmudhasan.mm07@gmail.com>
2024-12-29 16:01:04 +0600

    added navbar

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
 'use client'
+import { lora } from '@/app/fonts/fonts';
 import React from 'react';
+import { Routes } from '../Route/Route';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+
+    const pathName = usePathname()
     return (
         <div>
-            
+            <div>
+                <h1 className={``}>Magura</h1>
+            </div>
+            <div>
+                <ul>
+                    {
+                        Routes.map((e, idx) =>
+                            <Link href={e.route} className={`${pathName == e.route ? "text-accent font-semibold" : "text-white"}`} ><li>{e.label}</li></Link>
+                        )
+                    }
+                </ul>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
---
+++ src/components/shared/Route/Route.ts
@@ -0,0 +1,18 @@
+export const Routes =[
+    {
+        "label": "HOME",
+        "route": "/"
+    },
+    {
+        "label": "ABOUT",
+        "route": "/about"
+    },
+    {
+        "label": "SERVICES",
+        "route": "/services"
+    },
+    {
+        "label": "TESTIMONIALS",
+        "route": "/testimonials"
+    }
+]
