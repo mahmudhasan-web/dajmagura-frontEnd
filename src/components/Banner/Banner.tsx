282dc759e35dd4cc4c881c3e80964e02165e4f0e
Tahsin0909 <tajisan789@gmail.com>
2024-12-29 19:22:53 +0600

    completed res in banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,23 +7,23 @@ import banner2 from "@/assets/banner2.png"
 const Banner = () => {
     return (
         <section>
-            <div className='flex'>
+            <div className='grid lg:grid-cols-2'>
                 <div className='flex1 relative'>
-                    <Image src={banner1} alt='banner1'></Image>
+                    <Image src={banner1} alt='banner1' width={1200} height={820} className='w-full h-[300px] md:h-full'></Image>
                     <div className='w-10/12 mx-auto absolute top-1/4 border pl-12 pt-12 pb-12 pr-6 text-white left-[10%] space-y-6'>
-                        <h1 className='text-6xl'>Maximize Your Property’s Potential with Expert Management.</h1>
-                        <h3 className='text-3xl'>Let your property work for you. Earn more while we handle every detail effortlessly.</h3>
+                        <h1 className='md:text-6xl text-xl sm:text-2xl'>Maximize Your Property’s Potential with Expert Management.</h1>
+                        <h3 className='md:text-3xl text-lg sm:text-xl'>Let your property work for you. Earn more while we handle every detail effortlessly.</h3>
                         <p>Your trusted partner for hassle-free property management solutions.</p>
                     </div>
                     <div className='w-7/12 h-3 absolute top-[24%] mt-1 rounded-md bg-white left-[22%]'>
                     </div>
                 </div>
                 <div className='flex2'>
-                    <Image src={banner2} alt='banner2'></Image>
+                    <Image src={banner2} alt='banner2' width={1200} height={820} className='w-full h-[300px] md:h-full'></Image>
                 </div>
             </div>
-            
-        </section>
+
+        </section >
     );
 };
 
