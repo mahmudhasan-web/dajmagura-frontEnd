255b5a3b6e1be359f9646712d92eabb8e84e051e
Jibon <mailme.jibon@gmail.com>
2024-12-29 16:26:36 +0600

    header

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -4,7 +4,7 @@ import SectionHeader from "../shared/header/SectionHeader";
 const AboutUs = () => {
   return (
     <div>
-      <SectionHeader title="Our Story" />
+      <SectionHeader title="Our Story" line />
     </div>
   );
 };
