6169d66ead95e1ce114cb80f41beba1183a77b5c
Jibon <mailme.jibon@gmail.com>
2025-01-11 15:04:02 +0600

    update

---
+++ src/components/shared/header/Logo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import LogoIcon from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <Image
+        src={LogoIcon}
+        width={200}
+        className="w-[120px] hover:scale-105 transition-all"
+        height={220}
+        alt="logo"
+      />
+    </Link>
+  );
+};
+
+export default Logo;
